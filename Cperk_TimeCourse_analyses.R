eb679020d8afcfa72ad4e4828b842f4801915c12
John A Burns <burnsajohn@gmail.com>
2024-03-23 14:58:02 -0400

    Update Cperk_TimeCourse_analyses.R

--- Cperk_TimeCourse_analyses.R
+++ Cperk_TimeCourse_analyses.R
@@ -1,5 +1,4 @@
 ###libraries needed:
-
 library(tximport)
 library(edgeR)
 library(gplots)
@@ -8,7 +7,6 @@ library("e1071")
 library(ggplot2)
 library(reshape)
 library(dplyr)
-library(topGO)
 
 
 ###files needed#########################
@@ -83,8 +81,10 @@ y <- y[keep, ]
 # y is now ready for estimate dispersion functions see edgeR User's Guide
 y <- estimateDisp(y,design)
 
+###multiple dimensional scaling plot to visualize replicates. 
 plotMDS(y)
 
+#Fit a quasi-likelihood negative binomial generalized log-linear model to the data
 fit <- glmQLFit(y,design)
 
 #[1] "conditionF"    "conditionT104" "conditionT120" "conditionT54" 
@@ -178,163 +178,4 @@ p1 <- ggplot(Kmolten, aes(x=sample,y=value, group=cluster, colour=as.factor(clus
   labs(title= "Cluster Expression by Time",color = "Cluster")
 p1 + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-
-#######facet plot all clusters#########################
-###plot equivalent to Figure S2B in manuscript#########
-#######################################################
-
-i=1
-#Subset the cores molten dataframe so we can plot the core
-core2 <- Kmolten[Kmolten$cluster==i,]
-core2$CLUSTER<-i
-
-#get cluster 1
-K2 <- (scaledata[names(kClusters[kClusters==i]),])
-#calculate the correlation with the core
-corscore <- function(x){cor(x,core2$value)}
-score <- apply(K2, 1, corscore)
-#get the data frame into long format for plotting
-K2molten <- melt(K2)
-colnames(K2molten) <- c('gene','sample','value')
-#add the score
-K2molten <- merge(K2molten,score, by.x='gene',by.y='row.names', all.x=T)
-colnames(K2molten) <- c('gene','sample','value','score')
-#order the dataframe by score
-#to do this first create an ordering factor
-K2molten$order_factor <- 1:length(K2molten$gene)
-#order the dataframe by score
-K2molten <- K2molten[order(K2molten$score),]
-#set the order by setting the factors
-K2molten$order_factor <- factor(K2molten$order_factor , levels = K2molten$order_factor)
-K2molten$CLUSTER<-i
-plotclusters<-K2molten
-coreclusters<-core2
-
-#get the remaining clusters
-for(i in 2:length(unique(Kmolten$cluster))){
-#Subset the cores molten dataframe so we can plot the core
-core2 <- Kmolten[Kmolten$cluster==i,]
-core2$CLUSTER<-i
-#get cluster 2
-K2 <- (scaledata[names(kClusters[kClusters==i]),])
-#calculate the correlation with the core
-corscore <- function(x){cor(x,core2$value)}
-score <- apply(K2, 1, corscore)
-#get the data frame into long format for plotting
-K2molten <- melt(K2)
-colnames(K2molten) <- c('gene','sample','value')
-#add the score
-K2molten <- merge(K2molten,score, by.x='gene',by.y='row.names', all.x=T)
-colnames(K2molten) <- c('gene','sample','value','score')
-#order the dataframe by score
-#to do this first create an ordering factor
-K2molten$order_factor <- 1:length(K2molten$gene)
-#order the dataframe by score
-K2molten <- K2molten[order(K2molten$score),]
-#set the order by setting the factors
-K2molten$order_factor <- factor(K2molten$order_factor , levels = K2molten$order_factor)
-K2molten$CLUSTER<-i
-
-plotclusters<-rbind(plotclusters,K2molten)
-coreclusters<-rbind(coreclusters,core2)
-
-}
-
-###plot clusters as barplots over time:
-plotclusters2<-plotclusters
-plotclusters2$time <- as.numeric(sapply(strsplit(as.character(plotclusters2$sample), "_"), function(x) as.numeric(x[2])))
-# Calculate medians
-medians <- plotclusters2 %>% dplyr::group_by(CLUSTER, time) %>% dplyr::summarise(median_value = median(value))
-# Merge medians back with original data for plotting
-plot_data <- merge(plotclusters2, medians, by = c("CLUSTER", "time"))
-
-# Create the plot
-p2 <- ggplot(plot_data, aes(x = as.factor(time), y = value)) +
-  geom_boxplot() +
-  geom_line(data = medians, aes(x = as.factor(time), y = median_value, group = CLUSTER), color = "blue") +
-  facet_grid(cols = vars(CLUSTER)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-
-# Print the plot Figure S2B
-print(p2)
-####################################################################
-
-##############################################################################################################
-###GO analyses of each cluster################################################################################
-##############################################################################################################
-
-###UID mapping based GO annotations:
-geneID2GO <- readMappings(file = "Nk52_Cperk_UID.goa", IDsep=";|,")
-
-
-#order clusters based on expression profiles:
-
-#Old Cluster 5 becomes Cluster 1
-#Old Cluster 2 remains Cluster 2
-#Old Cluster 1 becomes Cluster 3
-#Old Cluster 3 becomes Cluster 4
-#Old Cluster 4 becomes Cluster 5
-
-clust1<-names(kClusters[kClusters==5])
-clust2<-names(kClusters[kClusters==2])
-clust3<-names(kClusters[kClusters==1])
-clust4<-names(kClusters[kClusters==3])
-clust5<-names(kClusters[kClusters==4])
-
-myInterestingGenes <-list(clust1, clust2, clust3, clust4, clust5)
-
-###Complete GO enrichment analyses for each cluster:
-for(i in 1:length(myInterestingGenes)){
-pat<-i
-
-geneNames<-names(geneID2GO)
-geneList <- factor(as.integer(geneNames %in% myInterestingGenes[[pat]]))
-names(geneList) <- geneNames
-sum(as.numeric(as.character(geneList)))
-length(myInterestingGenes[[pat]])
-
-GOdataBP <- tryCatch(new("topGOdata", ontology = "BP", allGenes = geneList, annot = annFUN.gene2GO, gene2GO = geneID2GO), error=function(e) "error")
-resultFisherBP.weight <- runTest(GOdataBP, algorithm = "weight", statistic = "fisher") 
-allResBP<- GenTable(GOdataBP, weight = resultFisherBP.weight, orderBy = "weight", topNodes = 200, numChar=1000)
-allResWeighted<- allResBP[as.numeric(gsub("<","",allResBP[,"weight"]))<0.05,]
-#head(allResBP,10)
-allResWeighted
-
-
-gotblnm<-paste(pat,"_GOPBtbls.txt",sep="")
-
-write.table(allResWeighted,gotblnm,quote=F,sep="\t")
-
-library(plyr)
-###function to pull genes associated with each GOterm.
-getGOgenes<-function(allResTable,updown,GOdata){
-	GOtermGenes<-list()
-	for (i in 1:length(allResTable[,1])){
-		cmd<-paste("ls(attributes(attributes(attributes(GOdata)$graph)$nodeData)$data$`",allResTable[i,1],"`$genes)",sep="")
-		GOgenes<-eval(parse(text=cmd))
-		updowngenes<-intersect(GOgenes,updown)
-		GOtermGenes[[i]]<-updowngenes
-	}
-	names(GOtermGenes)<-allResTable[,1]
-	gogenesDF<-ldply(GOtermGenes,cbind)
-	colnames(gogenesDF)<-c("GO.ID","UNIgene")
-	return(gogenesDF)
-}
-
-allResupdown<-allResBP
-#filter GOterm by pvalue --column6 is weighted fisher test
-updownres<-allResupdown[as.numeric(gsub("<","",allResupdown[,6]))<0.05,]
-###get genes associated with sig GO terms
-
-goupdown<-getGOgenes(updownres,myInterestingGenes[[pat]],GOdataBP)
-
-gognnm<-goupdown
-for(i in 1:length(gognnm[,1])){
-gognnm[i,3]<-updownres[grep(gognnm[i,1],updownres[,1]),2]
-}
-
-###save GO enrichment tables
-gognfilenm<-paste(pat,"_GOdf.txt",sep="")
-write.table(gognnm,gognfilenm,quote=F,sep="\t")
-}
 
