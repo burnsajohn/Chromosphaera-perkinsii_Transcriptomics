613790c8aa2c06cdf175f6fcc99971c6115c964b
John A Burns <burnsajohn@gmail.com>
2024-03-24 21:24:53 -0400

    Update Figure_2C.R

--- Figure_2C.R
+++ Figure_2C.R
@@ -1,1 +1,2 @@
-tst
+###This code will make a plot similar to Figure 2C from the manuscript. 
+###It uses count data for C. perkinsii generated for this project and incoporates count data generated for Sphaeroforma arctica previously: 
