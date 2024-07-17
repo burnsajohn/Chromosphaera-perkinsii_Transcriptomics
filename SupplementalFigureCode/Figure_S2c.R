a07447c802856fdbf483d50a994992bcbb5697d8
John A Burns <burnsajohn@gmail.com>
2024-07-17 00:24:37 -0400

    Update Figure_S2c.R

--- SupplementalFigureCode/Figure_S2c.R
+++ SupplementalFigureCode/Figure_S2c.R
@@ -1,2 +1,2 @@
-Figure S2c can be made by running the code in maine Figure 2C, but changing the GO terms to:
+Figure S2c can be made by running the code in main Figure 2C, but changing the GO terms to:
 GO:0000226 (MT Organization); GO:0030154 (Cell differentiation); GO:0051301 (Cell Division)
